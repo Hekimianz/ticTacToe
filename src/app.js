238868df6cfb4273082fdd5d02a59c14608c98a1
Hekimianz <aramhekdesign@gmail.com>
2023-09-26 14:44:28 -0600

    Display winner message

--- src/app.js
+++ src/app.js
@@ -24,15 +24,17 @@ const gameController = (() => {
   };
   // Check for win func
   const checkWin = (board) => {
+    const winnerDisp = document.querySelector(".win--wrapper");
     if (board.every((v) => v === board[0] && v !== "")) {
       console.log("Win!");
       cells.forEach((cell) => (cell.style.pointerEvents = "none"));
+      winnerDisp.style.display = "flex";
     }
   };
   // Place markers and switch turns
   cells.forEach((cell, i) => {
     cell.addEventListener("click", () => {
-      if (cell.innerHTML !== "") return;
+      // if (cell.innerHTML !== "") return;
       gameBoard[i] = currentPlayer.playerMark;
       renderCells();
       currentPlayer == player1
@@ -43,9 +45,9 @@ const gameController = (() => {
       checkWin(gameBoard.slice(3, 6));
       checkWin(gameBoard.slice(6, 9));
       // Check for vertical wins
-      const col1 = gameBoard.filter((el, i) => i === 0 || i === 1 || i === 2);
-      const col2 = gameBoard.filter((el, i) => i === 3 || i === 4 || i === 5);
-      const col3 = gameBoard.filter((el, i) => i === 6 || i === 7 || i === 8);
+      const col1 = gameBoard.filter((el, i) => i === 0 || i === 3 || i === 6);
+      const col2 = gameBoard.filter((el, i) => i === 1 || i === 4 || i === 7);
+      const col3 = gameBoard.filter((el, i) => i === 2 || i === 5 || i === 8);
       checkWin(col1);
       checkWin(col2);
       checkWin(col3);
